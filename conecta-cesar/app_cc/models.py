15869864f72b9a2e7df293315bffb2e9d353c39a
Miguel <msb2@cesar.school>
2024-03-26 15:50:42 -0300

    avisos e navbar

--- conecta-cesar/app_cc/models.py
+++ conecta-cesar/app_cc/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Login(models.Model):
+    email=models.CharField(max_length=100, null=False)
+    password=models.CharField(max_length=100, null=False)
--- conecta-cesar/project_cc/settings.py
+++ conecta-cesar/project_cc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_cc.apps.AppCcConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
