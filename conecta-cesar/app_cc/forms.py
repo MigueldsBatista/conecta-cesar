9a58864951366ee8af154071f4b29dd7cf83c1aa
nandaord <142419456+nandaord@users.noreply.github.com>
2024-06-24 05:14:04 -0300

    Update forms.py

--- conecta-cesar/app_cc/forms.py
+++ conecta-cesar/app_cc/forms.py
@@ -1,8 +1,1 @@
-# No seu arquivo forms.py
-from django import forms
-from .models import ToDoList, ToDoItem
 
-class ToDoListForm(forms.ModelForm):
-    class Meta:
-        model = ToDoList
-        fields = ['title']  
