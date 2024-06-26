9ef17b44559581208f5349387660738609f76c91
KauaneMelo <131185678+KauaneMelo@users.noreply.github.com>
2024-06-26 03:24:02 -0300

    Update views.py

--- conecta-cesar/app_cc/views.py
+++ conecta-cesar/app_cc/views.py
@@ -1064,10 +1064,11 @@ def atividades_professor(request):
         _atividade = []
         realizacao_atividades = []
         print(atividades)
+        x = []
         for atividade in atividades:
             _atividade.append(atividade)
             print(atividades)
-            x = list(AtividadeFeita.objects.filter(atividade=atividade))
+            x.append(list(AtividadeFeita.objects.filter(atividade=atividade)))
             if not x:
                 realizacao_atividades.append(False)
             else:
